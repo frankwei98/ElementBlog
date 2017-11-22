869f554891ae924de36af37c4aa00b5b0cc009a6
Frank Wei <frank@frankwei.xyz>
2017-11-22 23:00:33 +0800

    fix test

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import Axios from 'Axios'
+import Axios from 'axios'
 
 Vue.use(ElementUI)
 Vue.prototype.$axios = Axios
